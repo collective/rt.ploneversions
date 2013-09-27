d79bc342bc7e8c34af5469eaad3d3ffbd1da2903
Alessandro Pisa <alessandro.pisa@redturtle.it>
2013-09-27 09:21:32 +0200

    Initial import

---
+++ rt/ploneversions/ploneversions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+VERSION_URL_TEMPLATE= "http://dist.plone.org/release/%s/versions.cfg"
+
+class CFGParser(object):
+    ''' Parse a URL
+    '''
+
+
+def main(return_tagdir=False):
+    """ This will try to fetch versions from dist.plone.org
+    """
+    return 'Yatta'
